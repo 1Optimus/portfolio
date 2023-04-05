8fbb177c28922f9ed11df08fd706cc3160746421
Ricardo <richard.perez561@gmail.com>
2023-04-04 21:46:41 -0500

    adding projects

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -8,7 +8,8 @@ import Jsontocsv from '../../assets/jsontocsv.png'
 import Onetimesecret from '../../assets/onetimesecret.png'  
 import Urlshortener from '../../assets/urlshortener.png'  
 import Residential from '../../assets/r3.png'  
-import Poke from '../../assets/Poke.png'  
+import GCP from '../../assets/GCP.jpg'  
+import Poke from '../../assets/Poke3.png'  
 const Portafolio = () => {
   const [activeModal, setActiveModal]= useState(false)
   const [dataImg, setDataImg]= useState('')
@@ -19,9 +20,9 @@ const Portafolio = () => {
       <div className='container portfolio_container'>
         <article className='portfolio_item'>
           <div className='portfolio_item-inage'>
-            <img src={Pomo} alt=""/>
+            <img className='imgs' src={Pomo} alt=""/>
           </div>
-          <h3>Pomodoro</h3>
+          <h2>Pomodoro</h2>
           <div className="portafolio-item-cta">
             <a href='https://github.com/1Optimus/Pomodoro' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>
             <a href='https://pomodoro-ecru-chi.vercel.app/' className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live demo</a>
@@ -29,9 +30,9 @@ const Portafolio = () => {
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_item-inage'>
-            <img src={Jsontocsv} alt=""/>
+            <img className='imgs' src={Jsontocsv} alt=""/>
           </div>
-          <h3>Json to CSV</h3>
+          <h2>Json to CSV</h2>
           <div className="portafolio-item-cta">
             <a href='https://github.com/1Optimus/jsontocsv' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>
             <a href='https://jsontocsv-1optimus.vercel.app/' className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live demo</a>
@@ -39,9 +40,9 @@ const Portafolio = () => {
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_item-inage'>
-            <img src={Formu} alt=""/>
+            <img className='imgs'  src={Formu} alt=""/>
           </div>
-          <h3>Form</h3>
+          <h2>Form</h2>
           <div className="portafolio-item-cta">
             <a href='https://github.com/1Optimus/Form' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>
             <a href='https://form-woad-five.vercel.app/' className='btn btn-primary' target="_blank" rel="noopener noreferrer">Live demo</a>
@@ -49,18 +50,19 @@ const Portafolio = () => {
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_item-inage'>
-            <img src={Poke} alt="" height="140px"/>
+            <img className='imgs' src={Poke} alt="" height="250px"/>
           </div>
-          <h3>Pokedex</h3>
+          <h2>Pokedex</h2>
           <div className="portafolio-item-cta">
             <a href='https://github.com/1Optimus/Pokedex' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>
+            <p className='btn btn-primary' onClick={()=>{setActiveModal(true);setDataImg(data[1])}}>Images</p>
           </div>
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_item-inage'>
-            <img src={Onetimesecret} alt=""/>
+            <img className='imgs' src={Onetimesecret} alt=""/>
           </div>
-          <h3>One time secret</h3>
+          <h2>One time secret</h2>
           <div className="portafolio-item-cta">
           <a href='https://github.com/1Optimus/onetimesecret' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>
           <p className='btn btn-primary' onClick={()=>{setActiveModal(true);setDataImg(data[0])}}>Images</p>
@@ -68,9 +70,9 @@ const Portafolio = () => {
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_item-inage'>
-            <img src={Urlshortener} alt=""/>
+            <img className='imgs' src={Urlshortener} alt=""/>
           </div>
-          <h3>Url shortener</h3>
+          <h2>Url shortener</h2>
           <div className="portafolio-item-cta">
             <a href='https://github.com/1Optimus/urlshortener' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>
             <p className='btn btn-primary' onClick={()=>{setActiveModal(true);setDataImg(data[2])}}>Images</p>
@@ -78,12 +80,22 @@ const Portafolio = () => {
         </article>
         <article className='portfolio_item'>
           <div className='portfolio_item-inage'>
-            <img src={Residential} alt=""/>
+            <img className='imgs' src={Residential} alt=""/>
           </div>
-          <h3>Residential web app</h3>
+          <h2>Residential web app</h2>
           <div className="portafolio-item-cta">  
-            <p className='btn' >Code it is secret</p>          
+            <a href='https://github.com/1Optimus/PG-Public' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>      
             <p className='btn btn-primary' onClick={()=>{setActiveModal(true);setDataImg(data[3])}}>Images</p>
+          </div>
+        </article>
+        <article className='portfolio_item'>
+          <div className='portfolio_item-inage'>
+            <img className='imgs' src={GCP} alt=""/>
+          </div>
+          <h2>Gallery web app</h2>
+          <div className="portafolio-item-cta">  
+            <a href='https://github.com/1Optimus/GCP-project' className='btn'  target="_blank" rel="noopener noreferrer">Github</a>        
+            <p className='btn btn-primary' onClick={()=>{setActiveModal(true);setDataImg(data[4])}}>Images</p>
           </div>
         </article>
       </div>
